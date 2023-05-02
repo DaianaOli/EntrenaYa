f4cf5431d258029bf8192e9c40ff56fd861a58be
Luz Daiana Olivares <97685217+DaianaOli@users.noreply.github.com>
2023-05-02 19:06:38 -0300

    Inicio de app EntrenaYa

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Ejercicios from './paginas/Ejercicios';
+import Rutinas from './paginas/Rutinas';
+import Usuarios from './paginas/Usuarios';
+import Registro from './paginas/Registro';
+import Login from './paginas/Login';
+import PanelUsuario from './paginas/Panel/PanelUsuario';
+
+
+function App() {
+  const isAuthenticated = localStorage.getItem('token') ? true : false;
+  console.log(isAuthenticated)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Registro />} isAuthenticated={isAuthenticated}/>
+        <Route path='/login' element={<Login/>} isAuthenticated={isAuthenticated}/>
+        <Route exact path="/panel/:id" element={<PanelUsuario isAuthenticated={isAuthenticated} /> }   />
+        <Route path="/ejercicios" element={<Ejercicios />} />
+        <Route path="/rutinas" element={<Rutinas />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
