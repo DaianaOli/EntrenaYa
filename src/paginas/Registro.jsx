3443634d8062f671fbc81af63e44f09e2c35df5e
Luz Daiana Olivares <97685217+DaianaOli@users.noreply.github.com>
2023-05-03 20:38:33 -0300

    feat: Registro para usuarios

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -15,9 +15,15 @@ const Registro = (isAuthenticated) => {
       email: document.getElementById('email').value,
       contraseña: document.getElementById('password').value,
       apellido: document.getElementById('surname').value,
+      altura: document.getElementById('altura').value,
+      peso: document.getElementById('peso').value,
+      sexo: document.getElementById('sexo').value
     }
     crearUsuario(usuario)
-    
+    .then(() => {
+        window.location.href = '/login'
+        }
+    )
   }
 
   const handleSubmitForm = (event) => {
@@ -27,12 +33,12 @@ const Registro = (isAuthenticated) => {
 
   return (
     <div className="w-full flex flex-wrap">
-        <div className="w-full md:w-1/2 flex flex-col">
+        <div className="w-full md:w-1/2 flex flex-col h-screen overflow-y-scroll">
             <div className="flex justify-center md:justify-start pt-4 md:pl-12 md:-mb-12">
                 <a href="#" className="bg-black text-white font-bold text-xl p-4" alt="Logo">Logo</a>
             </div>
-            <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-                <p className="text-center text-3xl">Join Us.</p>
+            <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32 ">
+                <p className="text-center text-3xl"> Bienvenido!</p>
                 <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleSubmitForm}>
                     <div className="flex flex-col pt-4">
                         <label htmlFor="name" className="text-lg">Nombre</label>
@@ -41,6 +47,21 @@ const Registro = (isAuthenticated) => {
                     <div className="flex flex-col pt-4">
                         <label htmlFor="surname" className="text-lg">Apellido</label>
                         <input type="text" id="surname" placeholder="Smith" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label htmlFor="altura" className="text-lg">Altura</label>
+                        <input type="number" id="altura" placeholder="175" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label htmlFor="peso" className="text-lg">Peso</label>
+                        <input type="number" id="peso" placeholder="75" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label htmlFor="sexo" className="text-lg">Sexo</label>
+                        <select id="sexo" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline">
+                            <option value="masculino">Masculino</option>
+                            <option value="femenino">Femenino</option>
+                        </select>
                     </div>
                     <div className="flex flex-col pt-4">
                         <label htmlFor="email" className="text-lg">Email</label>
