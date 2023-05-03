934c722832048bd254adfa716d4322d05ce10274
Luz Daiana Olivares <97685217+DaianaOli@users.noreply.github.com>
2023-05-03 20:37:27 -0300

    feat: verif. de auth. y envío de form

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -32,7 +32,7 @@ const Login = ({isAuthenticated}) => {
           </a>
         </div>
         <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-          <p className="text-center text-3xl">Welcome.</p>
+          <p className="text-center text-3xl">Bienvenido!</p>
           <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleSubmit}>
             <div className="flex flex-col pt-4">
               <label htmlFor="email" className="text-lg">
@@ -47,7 +47,7 @@ const Login = ({isAuthenticated}) => {
             </div>
             <div className="flex flex-col pt-4">
               <label htmlFor="contraseña" className="text-lg">
-                Password
+                Contraseña
               </label>
               <input
                 type="password"
@@ -60,14 +60,14 @@ const Login = ({isAuthenticated}) => {
               type="submit"
               className="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8"
             >
-              Log In
+              Iniciar sesion
             </button>
           </form>
           <div className="text-center pt-12 pb-12">
             <p>
-              Don't have an account?{" "}
+              No tienes una cuenta?{" "}
               <a href="/" className="underline font-semibold">
-                Register here.
+                Registrate aquí.
               </a>
             </p>
           </div>
@@ -80,7 +80,7 @@ const Login = ({isAuthenticated}) => {
     <div className="w-1/2 shadow-2xl">
         <img
             className="object-cover w-full h-screen hidden md:block"
-            src="https://source.unsplash.com/IXUM4cJynP0"
+            src="https://img.freepik.com/foto-gratis/instructora-fitness-femenina-que-usa-computadora-portatil-ensenar-clase-casa_23-2149320123.jpg?w=740&t=st=1683146272~exp=1683146872~hmac=21b1b1448886717ceaff75228151fe4e8f148187697f28e229d7fb02f1ba0bcc"
         />
     </div>
 </div>
